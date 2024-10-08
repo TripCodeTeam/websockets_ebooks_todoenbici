e5d76340f07c19089f8e656efdd5f838f2602aed
foultrip <davidvasquezmahecha@gmail.com>
2024-10-08 09:35:16 -0500

    chore: update docker and load envs

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,13 @@
 FROM node:18
 
 # Establece el directorio de trabajo dentro del contenedor
-WORKDIR /app
+WORKDIR /usr/src/app
 
 # Copia los archivos de dependencias
 COPY package*.json ./
+
+# Instala el CLI de NestJS globalmente
+RUN npm install -g @nestjs/cli
 
 # Instala las dependencias
 RUN npm install --production
@@ -17,7 +20,7 @@ COPY . .
 RUN npm run build
 
 # Expone el puerto que usará la aplicación
-EXPOSE 3000
+EXPOSE 8080
 
 # Comando para iniciar la aplicación
 CMD ["npm", "run", "start:prod"]
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { CORS } from "./constants/cors"
+import { CORS } from './constants/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get<number>('PORT') || 3000;
-  await app.listen(port);
   app.enableCors(CORS);
+  await app.listen(configService.get('PORT'));
 }
 bootstrap();
